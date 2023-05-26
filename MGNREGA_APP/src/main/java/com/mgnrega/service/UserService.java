27bd51781e656ceb97fdb7c9d4ac5bfca01a2b10
mevicky9254 <vikkichoudhary6528@gmail.com>
2023-05-26 23:23:12 +0530

    Login and Logout services

--- MGNREGA_APP/src/main/java/com/mgnrega/service/UserService.java
+++ MGNREGA_APP/src/main/java/com/mgnrega/service/UserService.java
@@ -1,11 +1,12 @@
 package com.mgnrega.service;
 
+import com.mgnrega.exception.UserException;
 import com.mgnrega.model.CurrentUserSession;
 import com.mgnrega.model.User;
 
 public interface UserService {
 	
-	public CurrentUserSession loginToAccount(User user) ;
-	public String logOutFromAccount(String key);
+	public CurrentUserSession loginToAccount(User user) throws UserException ;
+	public String logOutFromAccount(String key) throws UserException;
 
 }
